e952054bdb10eefbbfcbe62acb1f47b284242bd3
Parsa Karami <Parsakarami@live.com>
2024-01-01 10:12:36 -0500

    Initial Commit

---
+++ TTR-Multiplayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TTR-Multiplayer
+//
+//  Created by Parsa Karami on 2024-01-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TTR-Multiplayer/TTR_MultiplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTR_MultiplayerApp.swift
+//  TTR-Multiplayer
+//
+//  Created by Parsa Karami on 2024-01-01.
+//
+
+import SwiftUI
+
+@main
+struct TTR_MultiplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
