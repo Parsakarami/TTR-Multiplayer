5814412e518ba9e9cd12c4bf30cb1f317e79b7f5
Parsa Karami <Parsakarami@live.com>
2024-01-22 18:43:37 -0500

    Complete the claim point view user interface

--- TTR-Multiplayer/View/ClaimPointsView.swift
+++ TTR-Multiplayer/View/ClaimPointsView.swift
@@ -10,105 +10,144 @@ import SwiftUI
 struct ClaimPointsView: View {
     @StateObject var viewModel = ClaimPointsViewModel()
     var body: some View {
-        VStack (alignment:.center) {
-            Spacer()
-            Text("Total Points: \(viewModel.playerPoint.totalPoint)")
-                .foregroundColor(.white)
-                .frame(alignment: .center)
-                .padding([.leading,.trailing],80)
-                .padding([.top,.bottom], 10)
-                .background(.indigo)
-                .clipShape(.capsule)
-                .padding([.top,.bottom])
-            
-            if !viewModel.message.isEmpty {
-                Text(viewModel.message)
-                    .foregroundStyle(viewModel.isSuccessful ? .green : .red)
-            }
-            
-            Form {
-                Section("Trains") {
-                    generateStepper(property: $viewModel.oneTrain, number: 1)
-                    generateStepper(property: $viewModel.twoTrain, number: 2)
-                    generateStepper(property: $viewModel.threeTrain, number: 3)
-                    generateStepper(property: $viewModel.fourTrain, number: 4)
-                    generateStepper(property: $viewModel.fiveTrain, number: 5)
-                    generateStepper(property: $viewModel.sixTrain, number: 6)
-                    
-                    Toggle(isOn: $viewModel.playerPoint.isLongestPath) {
-                        Text("Longest path")
-                            .foregroundStyle(.indigo)
+        VStack {
+            VStack {
+                if !viewModel.isSuccessful {
+                    Button(action: {viewModel.updateClaim()})
+                    {
+                        HStack {
+                            Image(systemName: "checkmark")
+                            Text("Claim")
+                        }
+                        .foregroundStyle(.background)
+                        .frame(alignment: .center)
+                        .padding([.leading,.trailing],80)
+                        .padding([.top,.bottom], 10)
+                        .background(.green)
+                        .clipShape(.capsule)
                     }
-                    .padding(3)
+                    
+                    Text("Total points: \(viewModel.playerPoint.totalPoint)")
+                        .font(.body)
+                    
+                } else {
+                    Text("Total Points: \(viewModel.playerPoint.totalPoint)")
+                        .foregroundColor(.white)
+                        .frame(alignment: .center)
+                        .padding([.leading,.trailing],80)
+                        .padding([.top,.bottom], 10)
+                        .background(.indigo)
+                        .clipShape(.capsule)
+                }
+                
+                if !viewModel.message.isEmpty {
+                    Text(viewModel.message)
+                        .foregroundStyle(viewModel.isSuccessful ? .green : .red)
                 }
             }
+            .padding(.top,25)
+            
+            ScrollView {
+                VStack (alignment:.center) {
+                    Spacer()
+                    
+                    Text("Longest")
+                        .font(.headline)
+                        .frame(width:getScreenSize().width - 50 ,alignment: .leading)
+                    
                     VStack{
-                        ForEach(viewModel.playerPoint.allTickets) { ticket in
-                            let claimed = viewModel.getClaimedTicketValue(forKey: ticket.id)
-                            
-                            HStack{
-                                Text("\(ticket.point)")
-                                Divider()
-                                    .padding([.top,.bottom])
-                                Text("\(ticket.origin) to \(ticket.destination)")
-                                Spacer()
-                                Button(action: {
-                                    viewModel.setClaimedTicketValue(false, forKey: ticket.id)
-                                }) {
-                                    Image(systemName: "xmark")
-                                }
-                                .padding(10)
-                                .background(claimed == nil ? .gray : !claimed! ? .red : .gray)
-                                .clipShape(.circle)
-                                .foregroundStyle(.white)
-                                
-                                Button(action: {
-                                    viewModel.setClaimedTicketValue(true, forKey: ticket.id)
-                                }) {
-                                    Image(systemName: "checkmark")
-                                }
-                                .padding(10)
-                                .background(claimed == nil ? .gray : claimed! ? .green : .gray)
-                                .clipShape(.circle)
-                                .foregroundStyle(.white)
-                            }
-                            .frame(maxHeight: 55)
-                            .padding(5)
-                            .background(.white)
-                            .clipShape(RoundedRectangle(cornerRadius: 14))
-                            .padding(5)
-                            
-                            if viewModel.playerPoint.allTickets.last!.id != ticket.id {
-                                Divider()
-                            }
+                        Toggle(isOn: $viewModel.playerPoint.isLongestPath) {
+                            Text("Do you claim express ticket?")
                         }
                     }
-                    .frame(maxWidth: getScreenSize().width - 50)
-                    .padding(10)
-                    .background(.gray.opacity(0.1))
-                    .foregroundStyle(.indigo)
-            
-            if !viewModel.isSuccessful {
-                RoundedTTRButton(action: {
-                    viewModel.updateClaim()},
-                                 title: "Claim Points",
-                                 icon: "checkmark")
+                    .padding()
+                    .background(.background)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    
+                    
+                    Text("Trains")
+                        .font(.headline)
+                        .frame(width:getScreenSize().width - 50 ,alignment: .leading)
+                        .padding(.top)
+                    VStack {
+                        generateStepper(property: $viewModel.oneTrain, number: 1)
+                        generateStepper(property: $viewModel.twoTrain, number: 2)
+                        generateStepper(property: $viewModel.threeTrain, number: 3)
+                        generateStepper(property: $viewModel.fourTrain, number: 4)
+                        generateStepper(property: $viewModel.fiveTrain, number: 5)
+                        generateStepper(property: $viewModel.sixTrain, number: 6)
+                    }
+                    .padding()
+                    .background(.background)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    
+                    
+                    if viewModel.playerPoint.allTickets.count > 0 {
+                        Text("Tickets")
+                            .font(.headline)
+                            .frame(width:getScreenSize().width - 50 ,alignment: .leading)
+                            .padding(.top)
+                        
+                        VStack{
+                            ForEach(viewModel.playerPoint.allTickets) { ticket in
+                                let claimed = viewModel.getClaimedTicketValue(forKey: ticket.id)
+                                HStack{
+                                    Text("\(ticket.point)")
+                                    Divider()
+                                        .padding([.top,.bottom])
+                                    Text("\(ticket.origin) to \(ticket.destination)")
+                                    Spacer()
+                                    Button(action: {
+                                        viewModel.setClaimedTicketValue(false, forKey: ticket.id)
+                                    }) {
+                                        Image(systemName: "xmark")
+                                    }
+                                    .padding(10)
+                                    .background(claimed == nil ? .gray : !claimed! ? .red : .gray)
+                                    .clipShape(.circle)
+                                    .foregroundStyle(.white)
+                                    
+                                    Button(action: {
+                                        viewModel.setClaimedTicketValue(true, forKey: ticket.id)
+                                    }) {
+                                        Image(systemName: "checkmark")
+                                    }
+                                    .padding(9)
+                                    .background(claimed == nil ? .gray : claimed! ? .green : .gray)
+                                    .clipShape(.circle)
+                                    .foregroundStyle(.white)
+                                }
+                                .frame(maxHeight: 55)
+                                if viewModel.playerPoint.allTickets.last!.id != ticket.id {
+                                    Divider()
+                                }
+                            }
+                        }
+                        .padding()
+                        .background(.background)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        
+                    } else {
+                        Spacer()
+                    }
+                }
             }
         }
+        .padding([.leading,.trailing], 20)
+        .background(.gray.opacity(0.22))
     }
     
     private func generateStepper(property: Binding<Int>, number: Int) -> some View {
         return HStack{
             Stepper(value: property, label:
-            {
+                        {
                 HStack{
                     generateTrainLabel(number: number)
                     Spacer()
                     Text("\(property.wrappedValue)")
                         .padding(.trailing,10)
                 }
-                .foregroundColor(.indigo)
-            })
+            }).font(.system(.body))
         }
     }
     
